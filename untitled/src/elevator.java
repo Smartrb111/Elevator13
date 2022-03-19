58d20c8c2833761401250c848b4ffc1f69333be9
Smartrb111 <100877090+Smartrb111@users.noreply.github.com>
2022-03-20 01:00:59 +0300

    Loef

--- untitled/src/elevator.java
+++ untitled/src/elevator.java
@@ -2,38 +2,33 @@ import java.util.Scanner;
 
 public class elevator {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int oldFlour = 1;
 
-        boolean isExit = false;
-        while (!isExit) {
-            System.out.println("Введите кнопку этажа,как закончите поездку напишите ВЫХОД");
-            int newFlour = console.nextInt();
+        int oldflour = 1;
 
-            if (newFlour > 10 || newFlour < 0) {
-                System.out.println("Вы не можете поехать на этот этаж");
-
-            } else if (newFlour > oldFlour) {
-                for (int i = oldFlour; i < newFlour; i++) {
-                    System.out.println("Вы на этаже " + i);
-
+        boolean isWorking = true;
+        while (isWorking) {
+            Scanner console = new Scanner(System.in);
+            System.out.println("Введите этаж");
+            int newflour = console.nextInt();
+            if(newflour > 10 || newflour <0){
+                System.out.println("Вы не можете поехать на этот эатж");
+            }else if(newflour>oldflour){
+                for(int i = oldflour; i < newflour; i++){
+                    System.out.println(i);
                 }
-                if (newFlour < oldFlour) {
-                    for (int i = oldFlour; i < newFlour; i--) {
-                        System.out.println("Вы на этаже " + i);
-                    }
-                    Scanner console1 = new Scanner(System.in);
-                    String line = console1.nextLine();
-                    if (line.equals("ВЫХОД")) {
-                        isExit = true;
-
-
-                    }
+            }if(newflour < oldflour){
+                for (int i = oldflour; i > newflour; i--){
+                    System.out.println(i);
                 }
-
-
             }
+            oldflour = newflour;
+            System.out.println("Вы на этаже "+ oldflour);
+            System.out.println("Закончить? 1 - да/2 - нет");
 
+            int choice = console.nextInt();
+            if (choice == 1) {
+                isWorking = false;
+            }
         }
     }
 }
